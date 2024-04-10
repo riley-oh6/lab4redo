77f78aca37558bb392477acc369b740285464707
rileyoh6 <27960+rileyoh6@users.noreply.github.mit.edu>
2024-04-10 14:06:59 -0400

    minor changes

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -6,7 +6,7 @@
         <p>{info.description}</p>
         <p class="year">c. {info.year}</p>
         {#if info.url}
-            <a href={info.url} target="_blank" rel="noopener noreferrer">Visit Project</a>
+            <a href={info.url} target="_blank">Visit Project</a>
         {/if}
     </div>
     
